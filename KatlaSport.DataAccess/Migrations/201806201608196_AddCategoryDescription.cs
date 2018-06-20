ce855b6b474e15a45d3708b02db5329406c536bd
Anastasiya Katomakhina <katomakhinaanastasiya@gmail.com>
2018-06-20 19:14:31 +0300

    Migrations for step1 fixed

--- KatlaSport.DataAccess/Migrations/201806201608196_AddCategoryDescription.cs
+++ KatlaSport.DataAccess/Migrations/201806201608196_AddCategoryDescription.cs
@@ -1,15 +1,14 @@
 namespace KatlaSport.DataAccess.Migrations
 {
-    using System;
     using System.Data.Entity.Migrations;
-    
+
     public partial class AddCategoryDescription : DbMigration
     {
         public override void Up()
         {
             RenameColumn(table: "dbo.product_categories", name: "creatted_utc", newName: "created_utc");
         }
-        
+
         public override void Down()
         {
             RenameColumn(table: "dbo.product_categories", name: "created_utc", newName: "creatted_utc");
