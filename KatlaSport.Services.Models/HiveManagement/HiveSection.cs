a7d0e1fd37fcbcb5bcd2594c311b72fb91356898
Anastasiya Katomakhina <katomakhinaanastasiya@gmail.com>
2018-06-26 09:55:09 +0300

    Merge step14. Added the ability to create a new storage section, edit and delete an existing section.

--- KatlaSport.Services.Models/HiveManagement/HiveSection.cs
+++ KatlaSport.Services.Models/HiveManagement/HiveSection.cs
@@ -8,8 +8,33 @@ namespace KatlaSport.Services.HiveManagement
     public class HiveSection : HiveSectionListItem
     {
         /// <summary>
+        /// Gets or sets a hive section identifier.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets a hive section name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets a hive section code.
+        /// </summary>
+        public string Code { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether a hive section is deleted.
+        /// </summary>
+        public bool IsDeleted { get; set; }
+
+        /// <summary>
         /// Gets or sets a timestamp when the hive section was updated last time.
         /// </summary>
         public DateTime LastUpdated { get; set; }
+
+        /// <summary>
+        /// Gets or sets a store hive ID.
+        /// </summary>
+        public int StoreHiveId { get; set; }
     }
 }
